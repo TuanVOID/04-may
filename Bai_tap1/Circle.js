f5754738c94e757584379a93abae674b28517b7b
TuanVOID <102146396+TuanVOID@users.noreply.github.com>
2022-05-04 16:23:46 +0700

    sua ti

---
+++ Bai_tap1/Circle.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Circle = void 0;
+var Circle = /** @class */ (function () {
+    function Circle() {
+    }
+    return Circle;
+}());
+exports.Circle = Circle;
